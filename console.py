c8872618383e68ec28c15f358ae56065e029c249
Patrick van Staveren <trick@vanstaveren.us>
2019-03-08 00:11:25 +0000

    now a standing console view pane of sorts

--- console.py
+++ console.py
@@ -12,20 +12,40 @@ recent = []
 rx_match = re.compile("^RX \d+ : RTTY \(.*\): (\$\$EAGLE.*\*....)$")
 #rx_match = re.compile("^RX")
 
+def clear_screen():
+    sys.stdout.write("\033[2J")
+
+last_matches = []
+partial = ""
+
 while True:
+    # read some data
     position = log.tell()
     line = log.readline()
     if not line:
         time.sleep(1)
+
+    # decide if it's a partial line or not
     if line.count("\n") == 0:
-        print(" . . .  : ", line)
+        partial = line.strip()
         log.seek(position)
         time.sleep(0.1)
-        sys.stdout.write("\033[F") # Cursor up one line
-        sys.stdout.write("\033[K") # Clear to the end of line
-        continue
-    line = line.strip()
-    if rx_match.match(line):
-        print("MATCH   : ", line)
     else:
-        print("        : ", line)
+        # if it's a full line, match it
+        line = line.strip()
+
+        if rx_match.match(line):
+            partial = ""
+            #print("MATCH   : ", line)
+            if len(last_matches) > 10:
+                last_matches = last_matches[1:]
+            last_matches.append(line)
+        else:
+            # what to do with non-matching lines?
+            pass
+
+    # now print all of the output assuming the screen is clear
+    for x in last_matches:
+        print(x)
+    print("Partial: ", partial)
+    clear_screen()
